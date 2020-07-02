b2aae289bb9c3f63f3328e281d0bdd3491149bd6
Ilias Shah <imiliasshah@gmail.com>
2020-07-03 01:07:39 +0600

    initial UI done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,82 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import bodyBg from "./assets/img/body-bg.jpg";
+import Header from "./components/Header";
+import {Container} from "react-bootstrap";
+import {WiDayLightning} from "react-icons/all";
 
 function App() {
   return (
-    <div className="App">
-      <Container>lorem ipsume</Container>
+    <div className="app" style={{backgroundImage: `url(${bodyBg})`}}>
+      <Header/>
+
+      <div className="upcoming-days">
+        <Container>
+          <div className="day-list">
+            <div className="day-item">
+              <div className="day-condition active">
+                <div className="day">Today</div>
+                <div className="weather-icon"><WiDayLightning/></div>
+                <div className="temp"><span>18 <sup>o</sup></span>/ <span>12 <sup>o</sup></span></div>
+                <div className="weather-condition">Clear Sky</div>
+              </div>
+            </div>
+
+            <div className="day-item">
+              <div className="day-condition">
+                <div className="day">Sun</div>
+                <div className="weather-icon"><WiDayLightning/></div>
+                <div className="temp"><span>18 <sup>o</sup></span>/ <span>12 <sup>o</sup></span></div>
+                <div className="weather-condition">Cloudy</div>
+              </div>
+            </div>
+
+            <div className="day-item">
+              <div className="day-condition">
+                <div className="day">Mon</div>
+                <div className="weather-icon"><WiDayLightning/></div>
+                <div className="temp"><span>18 <sup>o</sup></span>/ <span>12 <sup>o</sup></span></div>
+                <div className="weather-condition">Cloudy</div>
+              </div>
+            </div>
+
+            <div className="day-item">
+              <div className="day-condition">
+                <div className="day">Tue</div>
+                <div className="weather-icon"><WiDayLightning/></div>
+                <div className="temp"><span>18 <sup>o</sup></span>/ <span>12 <sup>o</sup></span></div>
+                <div className="weather-condition">Cloudy</div>
+              </div>
+            </div>
+
+            <div className="day-item">
+              <div className="day-condition">
+                <div className="day">Wed</div>
+                <div className="weather-icon"><WiDayLightning/></div>
+                <div className="temp"><span>18 <sup>o</sup></span>/ <span>12 <sup>o</sup></span></div>
+                <div className="weather-condition">Cloudy</div>
+              </div>
+            </div>
+
+            <div className="day-item">
+              <div className="day-condition">
+                <div className="day">Thu</div>
+                <div className="weather-icon"><WiDayLightning/></div>
+                <div className="temp"><span>18 <sup>o</sup></span>/ <span>12 <sup>o</sup></span></div>
+                <div className="weather-condition">Cloudy</div>
+              </div>
+            </div>
+
+            <div className="day-item">
+              <div className="day-condition">
+                <div className="day">Fri</div>
+                <div className="weather-icon"><WiDayLightning/></div>
+                <div className="temp"><span>18 <sup>o</sup></span>/ <span>12 <sup>o</sup></span></div>
+                <div className="weather-condition">Cloudy</div>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+import "../assets/scss/header.scss";
+import {WiDayLightning} from "react-icons/all";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Container>
+        <div className="header-widgets">
+          <div className="main-widget">
+            <p className="weather-icon"><WiDayLightning/></p>
+            <p className="weather-condition">Clear Sky</p>
+            <p className="location">Dhaka, BD</p>
+            <p className="weather-temp">39 <sup>o</sup> <small>Celsius</small></p>
+            {/*<p className="date">11 July, 2020</p>*/}
+          </div>
+
+
+          <div className="meta-info">
+            <div className="datetime">
+              <span className="time">10:30 pm</span>
+              <span className="date">Monday, 18 June 2020</span>
+              <div className="other-info">
+                <span className="info-title">Comfort Level</span>
+                <span className="info-item">Humidty: <strong>97%</strong></span>
+                <span className="info-item">Feels Like: <strong>34 <sup>o</sup></strong></span>
+              </div>
+              <div className="other-info">
+                <span className="info-title">Wind</span>
+                <span className="info-item">Direction: <strong>Southwest</strong></span>
+                <span className="info-item">Speed: <strong>12-28 km/h</strong></span>
+              </div>
+            </div>
+            <div className="search-location-form">
+              <input type="search" className="search-input" placeholder="Type city..."/>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </header>
+  )
+}
+
+export default Header;
